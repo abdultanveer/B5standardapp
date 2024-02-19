61788baade5681b8c1cb4a333e37934a12cb7e7d
abdul <abdul.tanveer@gmail.com>
2024-02-19 12:15:29 +0530

    lifecycle of activity

--- app/src/main/java/com/example/b5standardapp/HomeActivity.java
+++ app/src/main/java/com/example/b5standardapp/HomeActivity.java
@@ -3,12 +3,13 @@ package com.example.b5standardapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
     //lateinit var tvMessage:TextView
     TextView tvData;
-
+public static String TAG = HomeActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +18,36 @@ public class HomeActivity extends AppCompatActivity {
 
         String data = getIntent().getStringExtra("nkey");
         tvData.setText(data);
+        Log.i(TAG,"created");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(TAG,"started -visible");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"resumed -restore game state");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w(TAG,"paused -- save data");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG,"stopped");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"destroyed");
     }
 }
