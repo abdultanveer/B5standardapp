252d7bb45592287ef31f0d77c3aa9d2185debfef
abdul <abdul.tanveer@gmail.com>
2024-02-19 14:37:02 +0530

    debugging

--- app/src/main/java/com/example/b5standardapp/MainActivity.kt
+++ app/src/main/java/com/example/b5standardapp/MainActivity.kt
@@ -19,11 +19,20 @@ class MainActivity : AppCompatActivity() {
 
     fun handleClick(view: View) {
         tvMessage.text = "button signinn was clicked"
-        var hIntent = Intent(this,HomeActivity::class.java)
+        add(10,20)
+        throw NullPointerException("demo  exception")
+      /*  var hIntent = Intent(this,HomeActivity::class.java)
         hIntent.putExtra("nkey","abdul")
-        startActivity(hIntent)
+        startActivity(hIntent)*/
     //kotlinn is typeless --  infer the datatype
         //Intent hIntent = new Intent()
+
+    }
+
+    private fun add(i: Int, i1: Int) :Int{
+        var d  = i + 30
+
+        return i1 + i
 
     }
 }
