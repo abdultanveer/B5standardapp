c5d4fa64a9cad0883af8346b238e7aeb436ff52f
abdul <abdul.tanveer@gmail.com>
2024-02-20 12:44:48 +0530

    room table  entity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    //room dependencies
+    implementation("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-common:2.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/b5standardapp/data/Item.kt
@@ -0,0 +1,13 @@
+package com.example.b5standardapp.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "item")
+data class Item(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val itemName: String,
+    val itemPrice: Double,
+    val quantityInStock: Int
+)
