3f8cd46b577bdc53ebd4350e9485867bee72626c
abdul <abdul.tanveer@gmail.com>
2024-02-20 12:47:37 +0530

    java to kotlin - concise

---
+++ app/src/main/java/com/example/b5standardapp/kotlinexs/Student.kt
@@ -0,0 +1,9 @@
+package com.example.b5standardapp.kotlinexs
+
+class Student(
+    var sid: Int,
+    var name: String,
+    var isEligible: Boolean,
+    var stipend: Float,
+    var address: String
+)
